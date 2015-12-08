6a095fc76e34a7bd13f1ededa00fcb18d504c837
kaylabrinkerhoff <brinkerhoff_kayla@yahoo.com>
2015-12-08 13:58:12 -0600

    Contact form resize

---
+++ php/contactthanks.php
@@ -0,0 +1,40 @@
+  <!doctype html>
+  <html class="no-js" lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Kayla Brinkerhoff Design</title>
+      <link rel="stylesheet" href="css/custom.css" />
+      <link rel="stylesheet" href="css/foundation.css" />
+</head>
+<body> 
+    
+    <!-- c. Right Nav Section -->
+    
+<div class="row">
+  <div class="large-10 columns intro-text">
+    <h2 class="above-row-margin"> 
+    Your message has been sent!
+  
+
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script> 
+<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+    
+      
+<script src="../js/vendor/jquery.js"></script> 
+<script src="../js/foundation/foundation.js"></script> 
+<script>
+
+    $(document).foundation();
+
+</script>
+
+
+
+</body>
+</html>
